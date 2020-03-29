a86cde1e241862dd382d1f3fd324a2abdb240a12
Sheri Kwong <sherikwong@gmail.com>
2020-03-29 00:43:50 -0400

    Moved routes

--- db/routes/route/items.js
+++ db/routes/route/items.js
@@ -1,6 +1,6 @@
 const Router = require('express');
 const router = Router();
-const models = require('../models');
+const models = require('../../models');
 const Sequelize = require('sequelize')
 
 

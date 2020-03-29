b2fa8f095d87a6328974b84ef12c33de9ffc4f1d
Sheri Kwong <sherikwong@gmail.com>
2020-03-29 19:25:34 -0400

    Added working client ID

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -6,11 +6,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from '@angular/router';
 import { AccountComponent } from './components/account/account.component';
+import {Google} from '../../../../secret';
 
 let config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+    provider: new GoogleLoginProvider(Google.id)
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
--- src/app/modules/account/components/login/login.component.ts
+++ src/app/modules/account/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, GoogleLoginProvider } from 'angularx-social-login';
 
 @Component({
   selector: 'okay-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  signIn(): void {
+    console.log('Hello', GoogleLoginProvider.PROVIDER_ID);
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
 }
